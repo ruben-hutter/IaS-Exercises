1933116e7e6a13555837690b33af1607b005f1ee
Tobias Hafner <Tobias_Hafner@live.de>
2022-03-30 15:02:31 +0200

    add exercise id to builder.py

--- builder.py
+++ builder.py
@@ -1,13 +1,14 @@
 import os
 import os.path
 import subprocess
+import sys
 from shutil import make_archive
 from shutil import move
 
 SUB_HEADER = 'ias-u'
 PART_NAMES = 'tobiashafner-rubenhutter'
 
-def crawl_directory(work_dir):
+def crawl_directory(work_dir, en=-1):
 	"""Crawls the directory for subfolders to process.
 
         All subfolders on the first layer named ias-u<num> are processed.
@@ -15,12 +16,17 @@ def crawl_directory(work_dir):
         the compression of the directoy itself an automatic push to github.
         """
 
+	if len(sys.argv) > 1:
+		en = sys.argv[1]
+
 	for sub_dir in next(os.walk(work_dir))[1]:
 		if sub_dir[:5] != SUB_HEADER:
 			continue
 
-		print('Processing', sub_dir, '...')
 		ex_num = sub_dir[5:]
+		if (en!=-1 and ex_num!=en):
+			continue
+		print('Processing', sub_dir, '...')
 		sub_path = os.path.join(work_dir, sub_dir)
 		md_path = os.path.join(sub_path, 'answers.md')
 		pdf_path = os.path.join(sub_path, 'answers.pdf')
@@ -53,8 +59,6 @@ def compress_directory(zip_path, sub_path, work_dir):
 	directory.
 	"""
 	print('Creating zip-file...')
-	print(zip_path)
-	print(sub_path)
 	make_archive(zip_path, 'zip', sub_path)
 	move(os.path.join(work_dir, zip_path+'.zip'), os.path.join(sub_path, zip_path+'.zip'))
 	return
@@ -79,5 +83,4 @@ def push_github():
 if __name__ == "__main__":
 	# get directory the script was called from
 	work_dir = os.path.dirname(os.path.realpath(__file__))
-	# start crawling
 	crawl_directory(work_dir)
