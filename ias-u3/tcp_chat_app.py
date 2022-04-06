84977a874b2cf5a4a14af13972e5668720189ced
Tobias Hafner <Tobias_Hafner@live.de>
2022-04-06 16:40:21 +0200

    fixed bugs in tcp_chat_app

--- ias-u3/tcp_chat_app.py
+++ ias-u3/tcp_chat_app.py
@@ -2,16 +2,17 @@
 import sys
 import tcp_chat
 
-def main(ip_addr, port):
-	client = Client(ip_addr, port)
+def main(ip_addr, r_port, listen_port):
+	client = Client(ip_addr, r_port, listen_port)
 	client.connect()
 	client.start_sender()
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print ('Invalid arguments! \n Usage: ./tcp_chat_app.py <ip_address> <port>')
+	if len(sys.argv) != 4:
+		print ('Invalid arguments! \n Usage: ./tcp_chat_app.py <receiver_address> <receiver_port> <listening_port>')
 		exit()
 	ip_addr = sys.argv[1]
-	port = sys.argv[2]
-	main(ip_addr, port)
+	r_port = sys.argv[2]
+	listen_port = sys.argv[3]
+	main(ip_addr, r_port, listen_port)
 
