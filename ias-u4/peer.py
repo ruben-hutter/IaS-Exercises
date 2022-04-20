55fc83d71a211751349875df0e5ce1f369f05fa2
Tobias Hafner <Tobias_Hafner@live.de>
2022-04-20 22:24:25 +0200

    startet inplementation of routing table in peer

--- ias-u4/peer.py
+++ ias-u4/peer.py
@@ -1,1 +1,18 @@
 # peer node code goes here...
+
+# stores the routing table
+class Routing:
+	routing_table = {}
+
+	# returns the RTT from source to target node
+	# -1 is returned if infinite
+	def get_rtt(source_id, dest_id):
+		if source_id not in routing_table:
+			return -1
+		if dest_id not in routing_table[source_id]:
+			return -1
+		return routing_table[source_id][dest_id]
+
+	# sets the specified connections rtt in the routing table
+	def set_rtt(source_id, dest_id, rtt):
+		routing_table[source_id][dest_id] = rtt
