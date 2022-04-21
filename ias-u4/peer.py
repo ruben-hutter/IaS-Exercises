30a5e61976cdabc40a26652cc6b9136b69383bc6
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-21 21:04:43 +0200

    reorg

--- ias-u4/peer.py
+++ ias-u4/peer.py
@@ -7,6 +7,43 @@ class Protocol:
 	TOPOLOGY_UPDATE = 'NTU'
 	NETWORK_UPDATE = 'NU'
 
+# stores the routing table
+class Routing:
+
+	INFINITE = -1
+	NO_HOP = ''
+
+	node_id = ''
+	peer_addr = {}
+	routing_table = {}
+
+	# add entry to routing table
+	@staticmethod
+	def add_route(dest_id):
+		routing_table[dest_id] = [INFINITE, NO_HOP]
+
+	# returns the RTT to destination
+	# returns Routing.INFINITE if no route found
+	@staticmethod
+	def get_rtt(dest_id):
+		return routing_table[dest_id][0]
+
+	# set rtt to destination
+	@staticmethod
+	def set_rtt(dest_id, rtt):
+		routing_table[dest_id][0] = rtt
+
+	# return next hop to destination
+	# return ROUTING.NO_HOP if no route found
+	@staticmethod
+	def get_next_hop(dest_id):
+		return routing_table[dest_id][1]
+
+	# set next hop to destination
+	@staticmethod
+	def set_next_hop(dest_id, next_hop_id):
+		routing_table[dest_id][1] = next_hop_id
+
 # launch node
 def launch(ip_addr, port):
 	# create server socket to bind incomin connections
@@ -82,43 +119,6 @@ def parse_msg(message_tokens):
 # process nu
 def parse_nu(nu_tokens):
 	return
-
-# stores the routing table
-class Routing:
-
-	INFINITE = -1
-	NO_HOP = ''
-
-	node_id = ''
-	peer_addr = {}
-	routing_table = {}
-
-	# add entry to routing table
-	@staticmethod
-	def add_route(dest_id):
-		routing_table[dest_id] = [INFINITE, NO_HOP]
-
-	# returns the RTT to destination
-	# returns Routing.INFINITE if no route found
-	@staticmethod
-	def get_rtt(dest_id):
-		return routing_table[dest_id][0]
-
-	# set rtt to destination
-	@staticmethod
-	def set_rtt(dest_id, rtt):
-		routing_table[dest_id][0] = rtt
-
-	# return next hop to destination
-	# return ROUTING.NO_HOP if no route found
-	@staticmethod
-	def get_next_hop(dest_id):
-		return routing_table[dest_id][1]
-
-	# set next hop to destination
-	@staticmethod
-	def set_next_hop(dest_id, next_hop_id):
-		routing_table[dest_id][1] = next_hop_id
 
 
 '''
