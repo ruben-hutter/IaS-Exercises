f0a858bb592b72e540bcb029ca3055e59f6517bd
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-21 20:56:40 +0200

    added nu initialisation

--- ias-u4/peer.py
+++ ias-u4/peer.py
@@ -1,6 +1,12 @@
 # peer node code goes here...
 import socket
 
+# protocol message prefixes
+class Protocol:
+	MESSAGE = 'MSG'
+	TOPOLOGY_UPDATE = 'NTU'
+	NETWORK_UPDATE = 'NU'
+
 # launch node
 def launch(ip_addr, port):
 	# create server socket to bind incomin connections
@@ -11,10 +17,10 @@ def launch(ip_addr, port):
 	# wait for incoming connections
 	while True:
 		# accept connection from othe peer
-		peer_sock, peer_addr = sock.accept()
+		peer_sock, _ = sock.accept()
 		try:
 			# receive message from other peer
-			msg = connection.recv()
+			msg = peer_sock.recv()
 			if msg:
 				parse_input(msg)
 			else:
@@ -30,8 +36,10 @@ def parse_input(message):
 		parse_ntu(message.split(':'))
 		return
 	if message.startswith(Protocol.MESSAGE):
-		parse_message(message.split(':'))
+		parse_msg(message.split(':'))
 		return
+	if message.startswith(Protocol.NETWORK_UPDATE):
+		parse_nu(message.split(':'))
 
 # run ntu
 def parse_ntu(ntu_tokens):
@@ -50,7 +58,7 @@ def parse_ntu(ntu_tokens):
 			continue
 		addr_tokens = addr.split('_')
 		# add peer addr to peer addersses
-		peer_addr[addr_tokens[0]] = (addr_tokens[1],int(addr_tokens[2]))
+		Routing.peer_addr[addr_tokens[0]] = (addr_tokens[1],int(addr_tokens[2]))
 	# process peer links
 	links = ntu_tokens[2]
 	for link in links.split(','):
@@ -63,18 +71,17 @@ def parse_ntu(ntu_tokens):
 
 # process message
 def parse_msg(message_tokens):
-	if message_tokens[2] == node_id:
-		msg = ' '.join(message_tokens[3:]
+	if message_tokens[2] == Routing.node_id:
+		msg = ' '.join(message_tokens[3:])
 		print(f'Message received from node {message_tokens[1]}:')
 		print(msg)
 		return
-	message_tokens[1] = node_id
-	forward_msg(':'.join(message_tokens))
+	message_tokens[1] = Routing.node_id
+	#forward_msg(':'.join(message_tokens))
 
-# protocol message prefixes
-class Protocol:
-	MESSAGE = 'MSG'
-	TOPOLOGY_UPDATE = 'NTU'
+# process nu
+def parse_nu(nu_tokens):
+	return
 
 # stores the routing table
 class Routing:
@@ -87,34 +94,32 @@ class Routing:
 	routing_table = {}
 
 	# add entry to routing table
+	@staticmethod
 	def add_route(dest_id):
 		routing_table[dest_id] = [INFINITE, NO_HOP]
 
 	# returns the RTT to destination
-	# returns Routing.INFINITE  if no route found
+	# returns Routing.INFINITE if no route found
+	@staticmethod
 	def get_rtt(dest_id):
 		return routing_table[dest_id][0]
 
 	# set rtt to destination
+	@staticmethod
 	def set_rtt(dest_id, rtt):
 		routing_table[dest_id][0] = rtt
 
 	# return next hop to destination
-	# return ROUTING.NO_HOP  if no route found
+	# return ROUTING.NO_HOP if no route found
+	@staticmethod
 	def get_next_hop(dest_id):
 		return routing_table[dest_id][1]
 
 	# set next hop to destination
+	@staticmethod
 	def set_next_hop(dest_id, next_hop_id):
 		routing_table[dest_id][1] = next_hop_id
 
-''' 	# update of least cost from x to y
-	def update_dist(rcv_id, rcv_vec):
-		for i in range(1,len(forwarding_table)):
-			if i == rcv_id:
-				continue
-			forwarding_table[0][i] = min(rcv_vec[i]+forwarding_table[0][rcv_id], forwarding_table[0][i])
-'''
 
 '''
 TODO
