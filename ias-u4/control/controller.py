914da0f130578d952190068e868a41b11587432d
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-22 14:31:47 +0200

    added description to controller

--- ias-u4/control/controller.py
+++
@@ -1,6 +0,0 @@
-import parser
-
-# update the networks topology according to the passed config file
-def topology_update(config_file):
-	# read config and update peers
-	parser.parse_file(config_file)
