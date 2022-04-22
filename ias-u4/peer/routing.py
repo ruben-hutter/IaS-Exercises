35235cdfdd5e3f9dfa3f223be68a94d05b9919cb
Tobias Hafner <Tobias_Hafner@live.de>
2022-04-22 14:31:31 +0200

    send_ntu

--- ias-u4/peer/routing.py
+++ ias-u4/peer/routing.py
@@ -31,7 +31,10 @@ def set_next_hop(dest_id, next_hop_id):
 
 # send nu to peers
 def send_nu(): # NU:origin_id:name1 rtt, ...
-	nu_msg = "NU:".join(dest_id) + ':'
+	nu_msg = "NU:" + node_id + ':'
 	for dest_id, rtt in routing_table:
+		# skip unreachable
+		if rtt == NO_HOP:
+			continue
 		nu_msg += dest_id + ' ' + rtt + ','
-	sender.send_msg(dest_id, nu_msg)
+	sender.broadcast_msg(nu_msg)
