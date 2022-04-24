b6d9a33c6518b712c669cc0041db346fd9e4415e
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-24 12:03:05 +0200

    term 8

--- ias-u4/terminator.py
+++ ias-u4/terminator.py
@@ -1,11 +1,13 @@
 # terminate all peers by pid
 import json
+import os
 
 def main():
     with open('sub_procs.json', 'r') as f:
         sub_procs = json.load(f)
     for _, pid in sub_procs.items():
-        print(f'kill: {pid}')
+        os.kill(int(pid), 9)
+        print(f'killed: {pid}')
 
 # file run
 if __name__ == "__main__":
