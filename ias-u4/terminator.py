59768aa1a0414936c3f80d4cde1cb2b361274612
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-24 11:52:30 +0200

    term 1

--- ias-u4/terminator.py
+++ ias-u4/terminator.py
@@ -4,7 +4,7 @@ import json
 def main():
     with open('sub_procs.json', 'r') as f:
         sub_procs = json.load(f)
-    for peer in sub_procs['peer_id']:
+    for peer in sub_procs:
         print(peer)
 
 # file run
