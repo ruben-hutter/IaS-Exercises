d01722c04f20e6050aeab4137b1687c69be7c430
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-24 11:51:41 +0200

    term

---
+++ ias-u4/terminator.py
@@ -0,0 +1,12 @@
+# terminate all peers by pid
+import json
+
+def main():
+    with open('sub_procs.json', 'r') as f:
+        sub_procs = json.load(f)
+    for peer in sub_procs['peer_id']:
+        print(peer)
+
+# file run
+if __name__ == "__main__":
+	main()
