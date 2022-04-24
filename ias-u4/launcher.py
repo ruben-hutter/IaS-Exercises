72b6066413a5aa6231bdff6e57fbabd97fe92117
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-04-24 10:47:19 +0200

    terminate processes

--- ias-u4/launcher.py
+++ ias-u4/launcher.py
@@ -18,16 +18,24 @@ def main(args):
 	elif modus == "peers":
 		# open config file
 		config = open(config)
+		# save all subprocesses
+		proc_table = {}
 		# read config and launch peers
 		for line in config:
 			# line is node declaration
 			if line.startswith('addr'):
 				# split line
 				line = line.split(':') # ["addr", "ip_addr", "port"]
+				peer_id = line[0][4:]
 				peer_addr = line[1]
 				peer_port = line[2]
 				# launch peer
-				subprocess.Popen([sys.executable, 'peer/peer.py', peer_addr, peer_port])
+				proc_table[peer_id] = subprocess.Popen([sys.executable, 'peer/peer.py', peer_addr, peer_port])
+	# terminate all subprocesses
+	for proc in proc_table:
+		outs, errs = proc.communicate()
+		print(f'outs: {outs} | errs: {errs}')
+		proc.terminate()
 
 # file run
 if __name__ == "__main__":
